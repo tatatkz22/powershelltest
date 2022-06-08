bea2cd7562507f349422e04cc4c0d955e6f0adcd
Tom <tom.zimba@datamate.co.za>
2022-06-08 18:47:40 +0200

    My first commit

---
+++ src/arrays/arrayD.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class arrayD {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String [] name;
+        name = new String[5];
+        name[0] = "Thato0";
+        name[1] = "Thato1";
+        name[2] = "Thato2";
+        name[3] = "Thato3";
+        name[4] = "Thato4";
+
+
+        for(int i=0;i<=name.length;++i){
+
+            System.out.println(name[i]);
+        }
+       /* System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name[2]);
+        System.out.println(name[3]);
+        System.out.println(name[4]);
+        System.out.println(name[5]);*/
+
+    }
+}
---
+++ src/arrays/arrayTwoD.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class arrayTwoD {
+    public static void main(String[] args) {
+
+        String name [][] = new String[3][2];
+        name[0][0]="Mr";
+        name[1][0]="Mrs";
+        name[2][0]="Ms";
+        name[0][1]="Thato";
+        name[1][1]="Rambuda";
+        name[2][1]="Nkosi";
+
+        System.out.println(name[0][0] +" "+name[0][1]);
+
+
+    }
+
+
+}
